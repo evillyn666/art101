dbcf560198cca9bf484ed180ad7b6a8ff4b362ea
evillyn666 <146971008+evillyn666@users.noreply.github.com>
2023-11-02 12:24:42 -0700

    h

---
+++ lab7/js/lab7.js
@@ -0,0 +1,25 @@
+// Evelyn Anguiano
+
+// 2 November 2023
+
+
+// sortUserName - a function that takes user input and sorts the letters of their name 
+function sortUserName() {
+    var userName = window.prompt("Hi, please tell me your name.");
+    console.log("userName =", userName);
+    // split string to array
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    // sort the array
+    var nameArraySort = nameArray.sort();
+    // join string back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    return nameSorted;
+
+}
+
+// output
+document.writeln("Hey! I've fixed your name : ",
+    sortUserName(), "</br>"
+);
