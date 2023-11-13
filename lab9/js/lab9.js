0ea055dcc32e8e98cd503e798869f8b80caff1d7
evillyn666 <146971008+evillyn666@users.noreply.github.com>
2023-11-12 16:20:08 -0800

    h

---
+++ lab9/js/lab9.js
@@ -0,0 +1,16 @@
+/*
+ * Author: Evelyn Anguiano
+ * Created: 12 November 2023
+ */
+
+
+
+// add button to challenge section
+
+$(".minor-section").append("<button id= 'button-minor-section'>Click Me!</button>");
+
+// add a click listener to the results button
+$(".minor-section").click(function(){
+    alert ("Hello World!");
+
+});
