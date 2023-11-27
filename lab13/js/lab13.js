d64166465c0fc270b35aaad0270c51ee82bbc952
evillyn666 <146971008+evillyn666@users.noreply.github.com>
2023-11-26 20:39:27 -0800

    n

---
+++ lab13/js/lab13.js
@@ -0,0 +1,38 @@
+// FizzBuzz 
+// evelyn anguiano
+// november 2023
+
+
+
+function fizzBuzz() {
+
+    let oneLongString = "";
+
+    for (let num = 1; num <= 200; num++) {
+        // Check for multiples of 3, 5, and 7
+        if (num % 3 === 0 && num % 5 === 0 && num % 7 === 0) {
+            oneLongString += num + " FizzBuzzBoom!<br>";
+        } else if (num % 3 === 0 && num % 5 === 0) {
+            oneLongString += num + " FizzBuzz!<br>";
+        } else if (num % 3 === 0 && num % 7 === 0) {
+            oneLongString += num + " FizzBoom!<br>";
+        } else if (num % 5 === 0 && num % 7 === 0) {
+            oneLongString += num + " BuzzBoom!<br>";
+        } else if (num % 3 === 0) {
+            oneLongString += num + " Fizz!<br>";
+        } else if (num % 5 === 0) {
+            oneLongString += num + " Buzz!<br>";
+        } else if (num % 7 === 0) {
+            oneLongString += num + " Boom!<br>";
+        } else {
+            oneLongString += num + "<br>";
+        }
+    }
+
+    // 'output' div
+    $("#output").html(oneLongString);
+}
+
+// fizzBuzz function
+fizzBuzz();
+
